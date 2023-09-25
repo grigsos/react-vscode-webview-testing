269a5948ce4a9fbf580639aa0e1c5988e14ffac4
grigos <osipovgrisha.ru2@gmail.com>
2023-09-25 16:12:31 +0200

    change to minutes as a better example

--- src/webview/Test.tsx
+++ src/webview/Test.tsx
@@ -1,4 +1,3 @@
-// Test.tsx
 import * as React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -7,24 +6,18 @@ const Test: React.FunctionComponent = () => {
 
   const handleButtonClick = () => {
     const today = new Date();
-    const targetDate = new Date(today.getFullYear(), 8, 24); // September is month 8 (0-based index)
+    const currentMinute = today.getMinutes();
 
-    // Compare the current date with the target date
-    if (today.toDateString() === targetDate.toDateString()) {
-      // Navigate to the "HelloWorld" page
+    if (currentMinute % 2 === 0) {
       navigate('/')
-    } else {
-      // Handle the case where it's not September 25th, if needed
     }
   };
 
   return (
     <div>
       <h2>Test Usage</h2>
-      {/* Render the HTML specific to Test usage */}
       <p>This is the Test component.</p>
       
-      {/* Add a button to trigger navigation */}
       <button onClick={handleButtonClick}>Go to HelloWorld</button>
     </div>
   );
